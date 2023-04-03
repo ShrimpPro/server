e0b2ccb1de072fd823b0eb5a9835870b737e3072
syasyarazad <syasya.syakhrazad@gmail.com>
2023-04-04 00:30:37 +0700

    testing-keempat

--- __test__/payment.test.js
+++ __test__/payment.test.js
@@ -4,11 +4,11 @@ const PaymentController = require("../controllers/paymentController");
 
 describe('PaymentController', () => {
   describe('createInvoice', () => {
-    it('should create invoice and order correctly', async () => {
+    it('should create order with correct price if isPond is PREMIUM', async () => {
       const req = {
         body: {
           isPond: 'PREMIUM',
-          totalPond: 3
+          totalPond: 5
         },
         user: {
           id: 'user-id',
@@ -21,18 +21,19 @@ describe('PaymentController', () => {
       };
       const expectedInvoice = {
         id: 'invoice-id',
-        external_id: 'invoice-shrimPro-id-123456789',
-        amount: 130000,
+        external_id: expect.stringContaining('invoice-shrimPro-id-'),
+        amount: 150000,
         payer_email: 'user@example.com',
         description: 'invoice for ShrimPro',
         invoice_url: 'https://invoice.xendit.com/invoice-url'
       };
       const expectedOrder = {
-        totalPrice: 130000,
+        totalPrice: 150000,
         user: 'user-id',
         status: 'PENDING',
         invoice: 'https://invoice.xendit.com/invoice-url'
       };
+      
       Xendit.getXenditInvoice = jest.fn().mockResolvedValue(expectedInvoice);
       Order.create = jest.fn().mockResolvedValue(expectedOrder);
 
@@ -40,7 +41,7 @@ describe('PaymentController', () => {
 
       expect(Xendit.getXenditInvoice).toHaveBeenCalledWith({
         external_id: expect.stringContaining('invoice-shrimPro-id-'),
-        amount: 130000,
+        amount: 150000,
         payer_email: 'user@example.com',
         description: 'invoice for ShrimPro'
       });
@@ -66,7 +67,7 @@ describe('PaymentController', () => {
       };
       const expectedInvoice = {
         id: 'invoice-id',
-        external_id: 'invoice-shrimPro-id-123456789',
+        external_id: expect.stringContaining('invoice-shrimPro-id-'),
         amount: 0,
         payer_email: 'user@example.com',
         description: 'invoice for ShrimPro',
@@ -78,6 +79,7 @@ describe('PaymentController', () => {
         status: 'PENDING',
         invoice: 'https://invoice.xendit.com/invoice-url'
       };
+
       Xendit.getXenditInvoice = jest.fn().mockResolvedValue(expectedInvoice);
       Order.create = jest.fn().mockResolvedValue(expectedOrder);
 
@@ -93,51 +95,6 @@ describe('PaymentController', () => {
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith(expectedInvoice);
     });
-
-    it('should create order with correct price if isPond is BASIC and totalPond is 3', async () => {
-      const req = {
-        body: {
-          isPond:       'BASIC',
-          totalPond: 3
-        },
-        user: {
-          id: 'user-id',
-          email: 'user@example.com'
-        }
-      };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      };
-      const expectedInvoice = {
-        id: 'invoice-id',
-        external_id: 'invoice-shrimPro-id-123456789',
-        amount: 30000,
-        payer_email: 'user@example.com',
-        description: 'invoice for ShrimPro',
-        invoice_url: 'https://invoice.xendit.com/invoice-url'
-      };
-      const expectedOrder = {
-        totalPrice: 30000,
-        user: 'user-id',
-        status: 'PENDING',
-        invoice: 'https://invoice.xendit.com/invoice-url'
-      };
-      Xendit.getXenditInvoice = jest.fn().mockResolvedValue(expectedInvoice);
-      Order.create = jest.fn().mockResolvedValue(expectedOrder);
-    
-      await PaymentController.createInvoice(req, res);
-    
-      expect(Xendit.getXenditInvoice).toHaveBeenCalledWith({
-        external_id: expect.stringContaining('invoice-shrimPro-id-'),
-        amount: 30000,
-        payer_email: 'user@example.com',
-        description: 'invoice for ShrimPro'
-      });
-      expect(Order.create).toHaveBeenCalledWith(expectedOrder);
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith(expectedInvoice);
-    });  
   });
 
   describe('PaymentController', () => {
