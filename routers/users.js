1e7b41b00eba32a0fc7aa5feca272942f10bf2f0
Agiff <ghifarimohammadrahadian@gmail.com>
2023-04-04 22:23:08 +0700

    feat: add endpoint for expo token

--- routers/users.js
+++ routers/users.js
@@ -6,6 +6,7 @@ router.get('/', userController.getUsers);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.get('/current', authentication, userController.currentUser);
+router.patch('/expo', authentication, userController.expoToken);
 router.get('/:id', userController.getUserDetail);
 router.put('/:id', userController.updateUser);
 router.patch('/membership/:id', userController.updateMembership);
